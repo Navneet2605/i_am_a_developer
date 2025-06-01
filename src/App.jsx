c3b66f90adbbdfbcb9970d45a71e8bb055676ff4
Navneet2605 <navneetpcs3771@gmail.com>
2025-06-02 02:49:27 +0530

    Tailwind installation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      Hello there
+  <h1 className='text-red-900'> 
+    clo red 
+  </h1>
+     <div className="bg-red-500 text-blue-800"> test for color</div>
     </div>
   )
 }
