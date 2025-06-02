5caf6220f615a85a8ba27949685937346980de3b
Navneet2605 <navneetpcs3771@gmail.com>
2025-06-03 03:14:19 +0530

    feature cards and slider bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import LogoSection from './components/LogoSection.jsx';
+import NavBar from './components/NavBar.jsx';
+import FeatureCards from './sections/FeatureCards.jsx';
 import Hero from './sections/Hero.jsx';
 import ShowcaseSection from './sections/ShowcaseSection.jsx';
 
 const App = () => {
   return (
   <>
+  <NavBar/>
   <Hero/>
   <ShowcaseSection/>
+  <LogoSection/>
+  <FeatureCards/>
   </>
   )
 }
