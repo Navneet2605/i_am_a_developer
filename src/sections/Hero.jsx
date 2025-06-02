ccbb6dc48b442747e9ddb73fdf90dda206c6dd55
Navneet2605 <navneetpcs3771@gmail.com>
2025-06-03 01:04:45 +0530

    check for build

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import Button from "../components/Button";
 import HeroExperience from "../components/HeroModels/HeroExperience";
 import { words } from "../constants";
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap';
 
 const Hero = () => {
   return (
