884ac5742223464be397c6d8c3509c364466d1b4
Navneet2605 <navneetpcs3771@gmail.com>
2025-06-02 18:20:58 +0530

    header ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import Hero from './sections/Hero.jsx';
 
 const App = () => {
   return (
-    <div>
-  <h1 className='text-red-900'> 
-    clo red 
-  </h1>
-     <div className="bg-red-500 text-blue-800"> test for color</div>
-    </div>
+  <>
+  <Hero/>
+  </>
   )
 }
 
---
+++ src/components/HeroModels/HeroExperience.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroExperience = () => {
+  return (
+    <div>
+      Hero Experience
+    </div>
+  )
+}
+
+export default HeroExperience
